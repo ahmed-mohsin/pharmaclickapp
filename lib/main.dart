64cc35c3b43f78a28c5cf1795b9541742ae59df5
ahmed <ph.ahmedmohsin@gmail.com>
2021-06-10 23:57:22 +0200

    swipper

--- lib/main.dart
+++ lib/main.dart
@@ -184,14 +184,22 @@ class Updates extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         backgroundColor: Colors.green,
-        title: Text('آخر التحديثات',style: TextStyle(fontFamily: "arn",),),
+        title: Text(
+          'آخر التحديثات',
+          style: TextStyle(
+            fontFamily: "arn",
+          ),
+        ),
       ),
       body: SingleChildScrollView(
           child: Column(
         children: [
-          updateElemnt('الجديد فى الاصدار 6.5','تفعيل تخصيم المرتجعات من الديون عند تنفيذ ارجاع من العملاء- تحسين بيانات تقفيل المبيعات اليومية لتكون اوضح- تحسين فى مراجعة بيانات فواتير العملاء قبل الحفظ- امكانية مراجعة فواتير البيع من تقارير العملاء الكاملة- تحسين بيانات الاحصائيات العامة- تحسين البحث و الاضافة الى الطلبيات- تحسين اقسام النواقص وتفعيل اعادة التحميل من داخل البرنامج- تحسين عرض الكميات فى فواتير العملاء المطبوعة-امكانية تصدير النواقص الغير متوفرة الى اكسيل- تفعيل فلتر لمعرفة الادوية التى تحت نظام المخزون التلقائي و الغير مفعلة فى قسم الادوية'),
-          updateElemnt('الجديد فى الاصدار 6.4','التحكم فى سيستم الديون بالايقاف و التفعيل او وضع حد اقصي لكل العملاء او لعميل معين- اضافة اداة صيانة لفحص الاصناف المكررة وحذفها- تحسين عرض حسابات الشركات فى الشركات - قسم المشتروات- امكانية استعراض النواقص كلها بغض النظر عن حالة الطلب- امكانية استعراض النواقص اليدوية فقط- تفعيل امكانية اعادة فتح الفاتورة مع العلم انه عند فتح الفاتورة مرة اخري سيتم حذف الدين و المرتجع المتعلق بهذه الفاتورة- لا يمكن فتح فاتورة تم عمل اغلاق يومي بعد بيعها او تم دفع جزء من الدين المرتبط بها- غلق مشكلة تمرير باركود بالخطاء مكان كمية التكويد او الكمية المباعة- غلق مشكلة ادخال خصم تكويد بالسالب'),
-          updateElemnt('الجديد فى الاصدار 6.3','تفعيل متابعة تعديل المخزون يدويا وظهوره في حركة الصنف- تفعيل البيع بالسالب لجميع الاصناف بشكل افتراضي يمكنك تعديل الضبط من الاعدادت العامة- تفعيل تسجيل النواقص يدويا وتم اضافتها للقائمة السريعة اللي بتظهر تحت اسم الصنف- عند تسجيل النواقص يمكنك ربطها بالعملاء وسيصلك تنبيه عند توفر الصنف- تفعيل المتابعة الشهرية لطلبات العملاء- تفعيل اظهار واخفاء الضريبة و الموظف فى الفاتورة من الاعدادات- تفعيل امكانية حفظ باك اب فى مسار اضافي ثالث- تحسينات في عرض المخزون في الاقسام المختلفة'),
+          updateElemnt('الجديد فى الاصدار 6.5',
+              'تفعيل تخصيم المرتجعات من الديون عند تنفيذ ارجاع من العملاء- تحسين بيانات تقفيل المبيعات اليومية لتكون اوضح- تحسين فى مراجعة بيانات فواتير العملاء قبل الحفظ- امكانية مراجعة فواتير البيع من تقارير العملاء الكاملة- تحسين بيانات الاحصائيات العامة- تحسين البحث و الاضافة الى الطلبيات- تحسين اقسام النواقص وتفعيل اعادة التحميل من داخل البرنامج- تحسين عرض الكميات فى فواتير العملاء المطبوعة-امكانية تصدير النواقص الغير متوفرة الى اكسيل- تفعيل فلتر لمعرفة الادوية التى تحت نظام المخزون التلقائي و الغير مفعلة فى قسم الادوية'),
+          updateElemnt('الجديد فى الاصدار 6.4',
+              'التحكم فى سيستم الديون بالايقاف و التفعيل او وضع حد اقصي لكل العملاء او لعميل معين- اضافة اداة صيانة لفحص الاصناف المكررة وحذفها- تحسين عرض حسابات الشركات فى الشركات - قسم المشتروات- امكانية استعراض النواقص كلها بغض النظر عن حالة الطلب- امكانية استعراض النواقص اليدوية فقط- تفعيل امكانية اعادة فتح الفاتورة مع العلم انه عند فتح الفاتورة مرة اخري سيتم حذف الدين و المرتجع المتعلق بهذه الفاتورة- لا يمكن فتح فاتورة تم عمل اغلاق يومي بعد بيعها او تم دفع جزء من الدين المرتبط بها- غلق مشكلة تمرير باركود بالخطاء مكان كمية التكويد او الكمية المباعة- غلق مشكلة ادخال خصم تكويد بالسالب'),
+          updateElemnt('الجديد فى الاصدار 6.3',
+              'تفعيل متابعة تعديل المخزون يدويا وظهوره في حركة الصنف- تفعيل البيع بالسالب لجميع الاصناف بشكل افتراضي يمكنك تعديل الضبط من الاعدادت العامة- تفعيل تسجيل النواقص يدويا وتم اضافتها للقائمة السريعة اللي بتظهر تحت اسم الصنف- عند تسجيل النواقص يمكنك ربطها بالعملاء وسيصلك تنبيه عند توفر الصنف- تفعيل المتابعة الشهرية لطلبات العملاء- تفعيل اظهار واخفاء الضريبة و الموظف فى الفاتورة من الاعدادات- تفعيل امكانية حفظ باك اب فى مسار اضافي ثالث- تحسينات في عرض المخزون في الاقسام المختلفة'),
           SizedBox(
             height: 75,
           )
@@ -202,7 +210,7 @@ class Updates extends StatelessWidget {
 }
 
 class updateElemnt extends StatelessWidget {
-  String a,b;
+  String a, b;
 
   updateElemnt(this.a, this.b);
 
@@ -224,18 +232,26 @@ class updateElemnt extends StatelessWidget {
               children: [
                 Text(
                   a,
-                  style: TextStyle(color: Colors.pinkAccent,fontFamily: "arn",fontWeight: FontWeight.bold),
+                  style: TextStyle(
+                      color: Colors.pinkAccent,
+                      fontFamily: "arn",
+                      fontWeight: FontWeight.bold),
                 ),
                 ListView(
                   physics: NeverScrollableScrollPhysics(), shrinkWrap: true,
-                  children:
-                      b.split('-') // split the text into an array
-                          .map((String text) =>
-                              Padding(
-                                padding: const EdgeInsets.only(right: 7),
-                                child: Text(' # '+text,style: TextStyle(color: Colors.grey,fontFamily: "arn",),),
-                              )) // put the text inside a widget
-                          .toList(), // convert the iterable to a list
+                  children: b
+                      .split('-') // split the text into an array
+                      .map((String text) => Padding(
+                            padding: const EdgeInsets.only(right: 7),
+                            child: Text(
+                              ' # ' + text,
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontFamily: "arn",
+                              ),
+                            ),
+                          )) // put the text inside a widget
+                      .toList(), // convert the iterable to a list
                 )
               ],
             ),
@@ -292,19 +308,51 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   List<String> _images = List();
   List<String> _headers = List();
+  List<String> _features = List();
 
   @override
   void initState() {
+    _features
+      ..add(
+          'https://scontent.fcai21-1.fna.fbcdn.net/v/t1.6435-9/178368903_306612707501399_7927093819376549802_n.png?_nc_cat=108&ccb=1-3&_nc_sid=973b4a&_nc_ohc=UoALy6f3K40AX-fS4a2&_nc_ht=scontent.fcai21-1.fna&oh=6a90908125b375ea32ed7abd2f1c1dd2&oe=60E9E4B4')
+      ..add(
+          'https://scontent.fcai21-1.fna.fbcdn.net/v/t1.6435-9/151994890_268969037932433_7259992956132354806_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=973b4a&_nc_ohc=ZuPe1J9j-koAX-XY9uO&_nc_ht=scontent.fcai21-1.fna&oh=b0947f9ce32f72042e3b8b125412aeea&oe=60E8C161')
+      ..add(
+          'https://scontent.fcai21-1.fna.fbcdn.net/v/t1.6435-9/178368903_306612707501399_7927093819376549802_n.png?_nc_cat=108&ccb=1-3&_nc_sid=973b4a&_nc_ohc=UoALy6f3K40AX-fS4a2&_nc_ht=scontent.fcai21-1.fna&oh=6a90908125b375ea32ed7abd2f1c1dd2&oe=60E9E4B4')
+      ..add(
+          'https://scontent.fcai21-1.fna.fbcdn.net/v/t1.6435-9/146826091_259335388895798_347846442415628470_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=973b4a&_nc_ohc=VbcDHP6D408AX9dAq4S&_nc_ht=scontent.fcai21-1.fna&oh=67d7c9fa1f000651e4a99dc8feeb7204&oe=60E7B2E9');
+
     _images
-      ..add('https://scontent.fcai21-1.fna.fbcdn.net/v/t1.6435-9/178368903_306612707501399_7927093819376549802_n.png?_nc_cat=108&ccb=1-3&_nc_sid=973b4a&_nc_ohc=UoALy6f3K40AX-fS4a2&_nc_ht=scontent.fcai21-1.fna&oh=6a90908125b375ea32ed7abd2f1c1dd2&oe=60E9E4B4')
-      ..add('https://scontent.fcai21-1.fna.fbcdn.net/v/t1.6435-9/151994890_268969037932433_7259992956132354806_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=973b4a&_nc_ohc=ZuPe1J9j-koAX-XY9uO&_nc_ht=scontent.fcai21-1.fna&oh=b0947f9ce32f72042e3b8b125412aeea&oe=60E8C161')
-      ..add('https://scontent.fcai21-1.fna.fbcdn.net/v/t1.6435-9/178368903_306612707501399_7927093819376549802_n.png?_nc_cat=108&ccb=1-3&_nc_sid=973b4a&_nc_ohc=UoALy6f3K40AX-fS4a2&_nc_ht=scontent.fcai21-1.fna&oh=6a90908125b375ea32ed7abd2f1c1dd2&oe=60E9E4B4')
-      ..add('https://scontent.fcai21-1.fna.fbcdn.net/v/t1.6435-9/146826091_259335388895798_347846442415628470_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=973b4a&_nc_ohc=VbcDHP6D408AX9dAq4S&_nc_ht=scontent.fcai21-1.fna&oh=67d7c9fa1f000651e4a99dc8feeb7204&oe=60E7B2E9');
+      ..add(
+          'https://scontent.fcai21-1.fna.fbcdn.net/v/t1.6435-9/178368903_306612707501399_7927093819376549802_n.png?_nc_cat=108&ccb=1-3&_nc_sid=973b4a&_nc_ohc=UoALy6f3K40AX-fS4a2&_nc_ht=scontent.fcai21-1.fna&oh=6a90908125b375ea32ed7abd2f1c1dd2&oe=60E9E4B4')
+      ..add(
+          'https://scontent.fcai21-1.fna.fbcdn.net/v/t1.6435-9/151994890_268969037932433_7259992956132354806_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=973b4a&_nc_ohc=ZuPe1J9j-koAX-XY9uO&_nc_ht=scontent.fcai21-1.fna&oh=b0947f9ce32f72042e3b8b125412aeea&oe=60E8C161')
+      ..add(
+          'https://scontent.fcai21-1.fna.fbcdn.net/v/t1.6435-9/178368903_306612707501399_7927093819376549802_n.png?_nc_cat=108&ccb=1-3&_nc_sid=973b4a&_nc_ohc=UoALy6f3K40AX-fS4a2&_nc_ht=scontent.fcai21-1.fna&oh=6a90908125b375ea32ed7abd2f1c1dd2&oe=60E9E4B4')
+      ..add(
+          'https://scontent.fcai21-1.fna.fbcdn.net/v/t1.6435-9/146826091_259335388895798_347846442415628470_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=973b4a&_nc_ohc=VbcDHP6D408AX9dAq4S&_nc_ht=scontent.fcai21-1.fna&oh=67d7c9fa1f000651e4a99dc8feeb7204&oe=60E7B2E9');
     _headers
-      ..add('First Image')
-      ..add('Second Image')
-      ..add('Third Image')
-      ..add('Fourth Image');
+      ..add(
+          'البرنامج ادارة كاملة للصيدلية 100% بكل نواحيها التجارية و المالية ومتابعة العملاء و الشركات')
+      ..add(
+          'تحديث قاعدة البيانات لتحتوى على اكثر من 27 الف صنف بالاسعار الجديدة والباركود الدولي')
+      ..add(
+          'التحكم فى كل اقسام البرنامج بالتفعيل والايقاف مثال سيستم المخزون التلقائي و تجهيز الطلبيات و الحضور و الانصراف و الديون')
+      ..add(
+          ' امكانية ربط اي عدد من نقاط البيع بكل بساطة و فصل درج الكاشير لكل جهاز')
+      ..add('نظام ادارة المخزون حسب رغبة وامكانيات المستخدم Optimised Stock')
+      ..add(
+          'طورنا مستوي الامان فهتلاقي حفظ تلقائي لكل لمسة في البرنامج كل ثانية و كمان فى باك اب بيتاخد بشكل تلقائي ممكن تتحكم فى معدل الوقت بدءا من ساعة وحتي 12 ساعة وكمان تقدر تحفظ الباك اب يدويا في اي وقت')
+      ..add(
+          'اك اب Backup Files بتتحفظ في 3 اماكن مختلفة على جهازك ممكن تظبطه يحفظ على فلاشة او هارد خارجي ودا هيديلك امان اعلى وكمان استعادة باك اب لو احتجت تنزل ويندوز جديد بتكون فى اقل من دقيقة')
+      ..add(
+          'نظام تتبع طلبات العملاء و متابعة الاصناف المطلوبة وووصلها فى المخزون')
+      ..add(
+          'تحديد الصلاحيات لكل فئة فى الصيدلية كاشير او مسؤول او مدير وامكانية اتاحة او غلق اي خاصية لاي فئة')
+      ..add('اضافة و تعديل بكل بساطة للاصناف و الموردين والعملاء')
+      ..add(
+          'تقدر تحفظ اي تقرير فى البرنامج على اكسيل زى الطلبيات و النواقص و المبيعات و تقفيل الايام و الاحصائيات حتي بيانات المخزون')
+      ..add(' نظام توصيل للمنازل ومتابعة الطلب حتي عودة عامل التوصيل');
     super.initState();
   }
 
@@ -315,32 +363,79 @@ class _MainScreenState extends State<MainScreen> {
           expandedHeight: 250.0,
           floating: false,
           pinned: true,
+          backgroundColor: Colors.pinkAccent,
           flexibleSpace: FlexibleSpaceBar(
               centerTitle: true,
               title: Text("PharmaCLick",
                   style: TextStyle(
-                    color: Colors.pinkAccent,
+                    color: Colors.white,
                     fontSize: 15.0,
                   )),
-              background: Swiper(
-                itemCount: _images.length,
-                itemBuilder: (BuildContext context, int index) => Image.network(
-                  _images[index],
-                  fit: BoxFit.cover,
-                ),
-                autoplay: true,
+              background: Container(
+                decoration: BoxDecoration(
+                    color: Colors.pinkAccent,
+                    borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(4),
+                        bottomLeft: Radius.circular(4))),
+                height: 250,
+                child: Center(
+                    child: Text(
+                  'أول برنامج عملي لادارة الصيدليات',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'arn',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                )),
               )),
         ),
         SliverList(
           delegate: SliverChildBuilderDelegate(
-                  (context, index) => Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  height: 75,
-                  color: Colors.black12,
-                ),
-              ),
-              childCount: 10),
+              (context, index) => Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 4),
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 150,
+                          color: Colors.white,
+                          child: Directionality(
+                            textDirection: TextDirection.rtl,
+                            child: Swiper(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: _headers.length,
+                              itemBuilder: (BuildContext context, int index) =>
+                                  Container(
+                                child: Center(
+                                    child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    _headers[index],
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        color: Colors.green,
+                                        fontSize: 20,
+                                        fontFamily: 'arn',
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                )),
+                              ),
+                              autoplay: true,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8),
+                          child: Container(
+                            height: MediaQuery.of(context).size.height - 250,
+                            decoration: BoxDecoration(
+                                color: Colors.pinkAccent.withAlpha(100),
+                                borderRadius: BorderRadius.circular(15)),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+              childCount: 1),
         )
       ],
     );
