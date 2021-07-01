b8dca304de57ca3d27aaa5a68ea8ec0a964bd655
ahmed <ph.ahmedmohsin@gmail.com>
2021-07-01 17:38:16 +0200

    login screen

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -42,56 +42,95 @@ class _LoginScreenState extends State<LoginScreen> {
         height: MediaQuery.of(context).size.height,
         width: MediaQuery.of(context).size.width,
         child: Center(
+          child: SingleChildScrollView(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Please log in first \n To Get Your Pharmacy Data',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontFamily: 'arn',
-                      fontSize: 20,
-                      color: Colors.pink,
-                      shadows: <Shadow>[
-                        Shadow(
-                          offset: Offset(1, 1),
-                          blurRadius: 3.0,
-                          color: Color.fromARGB(255, 0, 0, 0),
-                        ),
-                        Shadow(
-                          offset: Offset(1.5, 1.5),
-                          blurRadius: 8.0,
-                          color: Colors.grey,
-                        ),
-                      ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Please log in first \n To Get Your Pharmacy Data',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'arn',
+                  fontSize: 20,
+                  color: Colors.pink,
+                  shadows: <Shadow>[
+                    Shadow(
+                      offset: Offset(1, 1),
+                      blurRadius: 3.0,
+                      color: Color.fromARGB(255, 0, 0, 0),
                     ),
-                  ),
+                    Shadow(
+                      offset: Offset(1.5, 1.5),
+                      blurRadius: 8.0,
+                      color: Colors.grey,
+                    ),
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 15),
-                  child: Directionality(
-                    textDirection: TextDirection.ltr,
-                    child: Form(
-                        key: _formKey,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            Padding(
-                              padding:
-                              EdgeInsets.symmetric(horizontal: 10, vertical: 8),
-                              child: TextFormField(
-                                controller: userName,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 15),
+              child: Directionality(
+                textDirection: TextDirection.ltr,
+                child: Form(
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 10, vertical: 8),
+                          child: TextFormField(
+                            controller: userName,
+                            autofocus: false,
+                            style: TextStyle(
+                                fontSize: 15.0, color: Colors.black),
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Username',
+                              filled: true,
+                              errorText:
+                                  _validate ? 'Value Can\'t Be Empty' : null,
+                              fillColor: Colors.grey[200],
+                              contentPadding: const EdgeInsets.only(
+                                  left: 14.0, bottom: 6.0, top: 8.0),
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.red),
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.grey),
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                            validator: (value) {
+                              if (value.isEmpty) {
+                                return 'Please enter some text';
+                              }
+                              return null;
+                            },
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          child: Stack(
+                            alignment: const Alignment(0, 0),
+                            children: <Widget>[
+                              TextFormField(
+                                controller: password,
+                                obscureText: _obscureText,
                                 autofocus: false,
-                                style:
-                                TextStyle(fontSize: 15.0, color: Colors.black),
+                                style: TextStyle(
+                                    fontSize: 15.0, color: Colors.black),
                                 decoration: InputDecoration(
                                   border: InputBorder.none,
-                                  hintText: 'Username',
+                                  hintText: 'password',
                                   filled: true,
-                                  errorText:
-                                  _validate ? 'Value Can\'t Be Empty' : null,
+                                  errorText: _validate
+                                      ? 'Value Can\'t Be Empty'
+                                      : null,
                                   fillColor: Colors.grey[200],
                                   contentPadding: const EdgeInsets.only(
                                       left: 14.0, bottom: 6.0, top: 8.0),
@@ -100,7 +139,8 @@ class _LoginScreenState extends State<LoginScreen> {
                                     borderRadius: BorderRadius.circular(10.0),
                                   ),
                                   enabledBorder: UnderlineInputBorder(
-                                    borderSide: BorderSide(color: Colors.grey),
+                                    borderSide:
+                                        BorderSide(color: Colors.grey),
                                     borderRadius: BorderRadius.circular(10.0),
                                   ),
                                 ),
@@ -111,104 +151,77 @@ class _LoginScreenState extends State<LoginScreen> {
                                   return null;
                                 },
                               ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.symmetric(horizontal: 10),
-                              child: Stack(
-                                alignment: const Alignment(0, 0),
-                                children: <Widget>[
-                                  TextFormField(
-                                    controller: password,
-                                    obscureText: _obscureText,
-                                    autofocus: false,
-                                    style: TextStyle(
-                                        fontSize: 15.0, color: Colors.black),
-                                    decoration: InputDecoration(
-                                      border: InputBorder.none,
-                                      hintText: 'password',
-                                      filled: true,
-                                      errorText: _validate
-                                          ? 'Value Can\'t Be Empty'
-                                          : null,
-                                      fillColor: Colors.grey[200],
-                                      contentPadding: const EdgeInsets.only(
-                                          left: 14.0, bottom: 6.0, top: 8.0),
-                                      focusedBorder: OutlineInputBorder(
-                                        borderSide: BorderSide(color: Colors.red),
-                                        borderRadius: BorderRadius.circular(10.0),
-                                      ),
-                                      enabledBorder: UnderlineInputBorder(
-                                        borderSide: BorderSide(color: Colors.grey),
-                                        borderRadius: BorderRadius.circular(10.0),
-                                      ),
-                                    ),
-                                    validator: (value) {
-                                      if (value.isEmpty) {
-                                        return 'Please enter some text';
-                                      }
-                                      return null;
-                                    },
-                                  ),
-                                  Positioned(
-                                      right: 8,
-                                      child: GestureDetector(
-                                          onTap: _toggle,
-                                          child: Icon(_obscureText
-                                              ? Icons.visibility
-                                              : Icons.visibility_off)))
-                                ],
-                              ),
-                            ),
-                            CheckboxListTile(
-                              activeColor: Colors.pink,
-                              title: Text(
-                                "Remember Me",
-                                style: TextStyle(
-                                    fontFamily: 'arn', color: Colors.grey),
-                              ),
-                              value: rememberMeCheckValue,
-                              onChanged: (newValue) {
-                                setState(() {
-                                  rememberMeCheckValue = newValue;
-                                  print('remember me >> '+newValue.toString());
-                                });
-                              },
-                              controlAffinity: ListTileControlAffinity
-                                  .leading, //  <-- leading Checkbox
-                            ),
-                            Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    vertical: 50, horizontal: 10),
-                                child: Container(
-                                  height: 50,
-                                  width: double.infinity,
-                                  child: RaisedButton(
-                                    color: Colors.green,
-                                    onPressed: () {
-                                      // Validate returns true if the form is valid, or false
-                                      // otherwise.
-                                      if (_formKey.currentState.validate()) {
-                                        // If the form is valid, display a Snackbar.
-                                        Scaffold.of(context).showSnackBar(SnackBar(
-                                            content: Text('Processing Data')));
-                                      }
-                                    },
-                                    child: Text(
-                                      'Submit',
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                    shape: RoundedRectangleBorder(
-                                        borderRadius:
+                              Positioned(
+                                  right: 8,
+                                  child: GestureDetector(
+                                      onTap: _toggle,
+                                      child: Icon(_obscureText
+                                          ? Icons.visibility
+                                          : Icons.visibility_off)))
+                            ],
+                          ),
+                        ),
+                        CheckboxListTile(
+                          activeColor: Colors.pink,
+                          title: Text(
+                            "Remember Me",
+                            style: TextStyle(
+                                fontFamily: 'arn', color: Colors.grey),
+                          ),
+                          value: rememberMeCheckValue,
+                          onChanged: (newValue) {
+                            setState(() {
+                              rememberMeCheckValue = newValue;
+                              print('remember me >> ' + newValue.toString());
+                            });
+                          },
+                          controlAffinity: ListTileControlAffinity
+                              .leading, //  <-- leading Checkbox
+                        ),
+                        Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 50, horizontal: 10),
+                            child: Container(
+                              height: 50,
+                              width: double.infinity,
+                              child: RaisedButton(
+                                color: Colors.green,
+                                onPressed: () {
+                                  // Validate returns true if the form is valid, or false
+                                  // otherwise.
+                                  if (_formKey.currentState.validate()) {
+                                    // If the form is valid, display a Snackbar.
+                                    Scaffold.of(context).showSnackBar(
+                                        SnackBar(
+                                            content:
+                                                Text('Processing Data')));
+
+
+                                  }else{
+                                    Scaffold.of(context).showSnackBar(
+                                        SnackBar(
+                                            content:
+                                            Text('Fill Required TextFields')));
+                                  }
+                                },
+                                child: Text(
+                                  'Submit',
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius:
                                         new BorderRadius.circular(18.0),
-                                        side: BorderSide(color: Colors.green)),
-                                  ),
-                                )),
-                          ],
-                        )),
-                  ),
-                )
+                                    side: BorderSide(color: Colors.green)),
+                              ),
+                            )),
+                      ],
+                    )),
+              ),
+            )
               ],
-            )),
+            ),
+          ),
+        ),
       ),
     );
   }
